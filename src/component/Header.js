d25edd20370dd9e3d6493a9080675bcadc01ff2a
amanda <amanda@devpipeline.com>
2023-08-06 00:51:44 -0400

    header

---
+++ src/component/Header.js
@@ -0,0 +1,54 @@
+import { BiMenu } from 'react-icons/bi';
+import { AiOutlineClose } from 'react-icons/ai';
+import React, { useEffect, useState } from 'react';
+import classes from './Header.module.scss';
+
+const Header = () => {
+    const [menuOpen, setMenuOpen] = useState(true);
+    const [size, setsize] = useState({
+        width: undefined,
+        height: undefined,
+    });
+    useEffect(() => {
+        const handleResize = () => {
+            setsize({
+                width: window.innerWidth,
+                height: window.innerHeight,
+            });
+        };
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+    useEffect(() => {
+        if (size.width > 768 && menuOpen) {
+            setMenuOpen(false);
+        }
+    }, [size.width, menuOpen]);
+    const menuToggleHandler = () => {
+        setMenuOpen((p) => !p);
+    };
+    return (
+        <header className={classes.header}>
+            <div className={classes.header__content}>
+                <h2 className={classes.header__content__logo}>navbar</h2>
+                <nav className={`${classes.header__content__nav} ${menuOpen ? classes.isMenu : ''}`}>
+                    <ul>
+                        <li>
+                            <a href="/">PageOne</a>
+                        </li>
+                        <li>
+                            <a href="/">PageTwo</a>
+                        </li>
+                        <li>
+                            <a href="/">PageThree</a>
+                        </li>
+                    </ul>
+                    <button>Contact me</button>
+                </nav>
+                <div className={classes.header__content__toggle}>{!menuOpen ? <BiMenu onClick={menuToggleHandler} /> : <AiOutlineClose onClick={menuToggleHandler} />}</div>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
