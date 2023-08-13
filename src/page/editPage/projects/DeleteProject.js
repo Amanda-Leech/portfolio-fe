b7924409251e5d9c1511af900b8664f1806ef8aa
amanda <amanda@devpipeline.com>
2023-08-13 16:22:34 -0400

    prodject crud non functional

--- src/page/editPage/projects/DeleteProject.js
+++ src/page/editPage/projects/DeleteProject.js
@@ -1,12 +1,38 @@
-import React, { useState } from 'react';
+import React from 'react';
 import axios from 'axios';
 
-function DeleteProject() {
-    return (
-        <div className="form-container">
-            <h1>Delete Projects</h1>
-        </div>
-    );
-}
+export default class PersonList extends React.Component {
+    state = {
+        projects: [],
+    };
 
-export default DeleteProject;
+    componentDidMount() {
+        axios.get(`http://localhost:5000/project`).then((res) => {
+            const projects = res.data;
+            this.setState({ projects });
+        });
+    }
+
+    render() {
+        return (
+            <ul>
+                <h1> Delete Projects </h1>
+                <br />
+                <br />
+                <br />
+                {this.state.projects.map((project) => (
+                    <li key={project.project_id}>
+                        <p> Title: {project.project_title}</p>
+                        <p> URL: {project.project_url}</p>
+                        <a href={project.git_url}> Git URL: {project.git_url}</a>
+                        <p> Info: {project.project_info}</p>
+                        <button>Delete</button>
+                        <br />
+                        <br />
+                        <br />
+                    </li>
+                ))}
+            </ul>
+        );
+    }
+}
